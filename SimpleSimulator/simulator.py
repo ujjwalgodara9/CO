716f58ea7264a5116d542bbb256867d854e995ba
ujjwalgodara9 <88847906+ujjwalgodara9@users.noreply.github.com>
2021-08-23 23:38:53 +0530

    Delete as

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 code = []
+pc = 0
 type_a = ("add",
           "sub",
           "mul",
@@ -38,8 +39,8 @@ opcode_dic = {'00000': 'add',
               '01000': 'rs',
               '01001': 'ls',
               '01010': 'xor',
-              '01011': 'Or',
-              '01100': 'And',
+              '01011': 'or',
+              '01100': 'and',
               '01101': 'not',
               '01110': 'cmp',
               '01111': 'jmp',
@@ -75,11 +76,9 @@ def plot(x, y):
         plt.ylabel('Address')
         plt.title('Memory Accesses v/s Cycles')
         plt.savefig("graph.png")
-        plt.show()
+        # plt.show()
     except:
         pass
-
-
 
 
 def toBinary(n, bits):
@@ -92,9 +91,9 @@ def toBinary(n, bits):
 def rf_dump():
     for x in registers:
         if x == "flags":
-            print(registers[x].zfill(16),end=" ")
+            print(registers[x].zfill(16), end=" ")
         else:
-            print(registers[x][2:].zfill(16),end=" ")
+            print(registers[x][2:].zfill(16), end=" ")
     print()
 
 
@@ -104,15 +103,23 @@ def mem_dump():
 
 
 def do_type_a(line, smbl):
+    instruction = opcode_dic[line[:5]]
     # first get name of mentioned registers
     t_r1, t_r2, t_r3 = registers_add[line[7:10]], registers_add[line[10:13]], registers_add[line[13:16]]
     res = eval("int(registers[t_r2],2)" + smbl + "int(registers[t_r3],2)")  # calc all a type instr
+    # print("working")
+    # print(res)
+    result = bin(res)
+    result = result[2:]
     if res > 65535 or res < 0:  # checking overflow
         registers.update({"flags": "1000"})  # set overflow flag
-        registers.update({t_r1: bin(0)})
+        if instruction == "sub":
+            registers.update({t_r1: bin(0)})
+        else:
+            registers.update({t_r1: (f"0b{result[-16:]}")})
     else:
         registers.update({"flags": "0000"})  # else reset flag
-        registers.update({t_r1: bin(res)})  # and update registers
+        registers.update({t_r1: (f"0b{result[-16:]}")})  # and update registers
 
 
 def do_type_b(line, smbl):
@@ -138,6 +145,7 @@ def do_type_c(line):
         registers.update({"r1": remainder})
     elif instruction == "not":
         val = registers[t_r2][2:]
+        val = val.zfill(16)
         new_val = "0b"
         for x in val:
             if x == "0":
@@ -159,31 +167,61 @@ def do_type_d(line):
     t_r1, mem_add = registers_add[line[5:8]], int(line[8:16], 2)
     instruction = opcode_dic[line[:5]]
     access_mem.append(mem_add)
+    # print(code[mem_add])
     if instruction == "ld":
-        registers.update({t_r1: bin(mem_add)})
+        registers.update({t_r1: bin(int(code[mem_add],2))})
     else:
         code[mem_add] = registers[t_r1][2:].zfill(16)
 
 
 def do_type_e(line):
+    global pc
     jmp_mem_add = int(line[8:16], 2)
     instruction = opcode_dic[line[:5]]
+    # print(jmp_mem_add)
+    # print(code.index(code[jmp_mem_add]))
     if instruction == "jmp":
-        execute(code[jmp_mem_add], code.index(jmp_mem_add))
+        registers.update({"flags": "0000"})  # reset flag
+        print(toBinary(pc, 8), end=" ")
+        rf_dump()
+        pc = jmp_mem_add
+        # execute(code[jmp_mem_add], code.index(code[jmp_mem_add]))
     elif instruction == "jlt" and registers["flags"] == "0100":
-        execute(code[jmp_mem_add], code.index(jmp_mem_add))
+        registers.update({"flags": "0000"})  # reset flag
+        print(toBinary(pc, 8), end=" ")
+        rf_dump()
+        pc = jmp_mem_add
+        # execute(code[jmp_mem_add], code.index(code[jmp_mem_add]))
     elif instruction == "jgt" and registers["flags"] == "0010":
-        execute(code[jmp_mem_add], code.index(jmp_mem_add))
+        registers.update({"flags": "0000"})  # reset flag
+        # print("jumping")
+        print(toBinary(pc, 8), end=" ")
+        rf_dump()
+        pc = jmp_mem_add
+        # rf_dump()
+        # execute(code[jmp_mem_add], code.index(code[jmp_mem_add]))
     elif instruction == "je" and registers["flags"] == "0001":
-        execute(code[jmp_mem_add], code.index(jmp_mem_add))
+        registers.update({"flags": "0000"})  # reset flag
+        print(toBinary(pc, 8), end=" ")
+        rf_dump()
+        pc = jmp_mem_add
+        # execute(code[jmp_mem_add], code.index(code[jmp_mem_add]))
+    else:
+        registers.update({"flags": "0000"})  # reset flag
+        print(toBinary(pc, 8), end=" ")
+        rf_dump()
+        pc += 1
 
 
-def execute(line, mem_index):
+def execute(line, add):
+    global pc
     # instruction = add, or, mul ....
     instruction = opcode_dic[line[:5]]
-    access_mem.append(mem_index)
+    access_mem.append(add)
+    # print(line)
     # print(instruction)
     if instruction in type_a:
+        # print("www")
         do_type_a(line, opcode_smbl[instruction])
     elif instruction in type_b:
         do_type_b(line, opcode_smbl[instruction])
@@ -191,11 +229,32 @@ def execute(line, mem_index):
         do_type_c(line)
     elif instruction in type_d:
         do_type_d(line)
-    elif instruction in type_e:
-        do_type_e(line)
-    if instruction not in ("cmp", "add"):
+    if instruction not in ("cmp", "add", "sub", "mul","jmp","jlt", "jgt","je"):
         registers.update({"flags": "0000"})  # reset flag
+    if instruction in ("add",
+          "sub",
+          "mul",
+          "xor",
+          "or",
+          "and",
+                       "movi",
+                       "rs",
+                       "ls","mov",
+          "div",
+          "not",
+          "cmp","ld",
+          "st"
+                       ):
+        # print("adding pc")
+        print(toBinary(add, 8), end=" ")
+        rf_dump()
+        pc += 1
+    if instruction in type_e:
+        do_type_e(line)
+
     if instruction == "hlt":
+        print(toBinary(add, 8), end=" ")
+        rf_dump()
         return True
     else:
         return False
@@ -203,6 +262,7 @@ def execute(line, mem_index):
 
 def main():
     global code
+    global pc
     cycles = []
     for x in range(256):
         code.append("0000000000000000")
@@ -217,17 +277,18 @@ def main():
         except:
             pass
         counter += 1
-    pc = 0
+    # pc = 0
     halted = False
     while not halted:
         line = code[pc]
         cycles.append(pc)
         halted = execute(line, pc)
+
         # print(halted)
         # halted = True
-        print(toBinary(pc, 8), end=" ")
-        rf_dump()
-        pc += 1
+        # print(toBinary(pc, 8), end=" ")
+        # rf_dump()
+        # pc += 1
     mem_dump()
     plot(cycles, access_mem)
 
